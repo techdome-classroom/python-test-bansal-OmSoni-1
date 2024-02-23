c37ff069629cd4b143d8608c7e0617077a632c7d
Om Soni <omsoni833@gmail.com>
2024-02-23 13:37:27 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,12 +1,25 @@
 class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-    
 
+    def isValid(self, s) -> bool:
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
+    
+# sol = Solution()
+# print(sol.isValid("(])"))
+
+
+
+
+
+            
 
 
   
--- program2.py
+++ program2.py
@@ -1,7 +1,17 @@
 class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+
+    def romanToInt(self, s) -> int:
+        roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        
+        prev_value = 0
+        for char in s:
+            current_value = roman_values[char]
+            if current_value > prev_value:
+                result += current_value - 2 * prev_value
+            else:
+                result += current_value
+            prev_value = current_value
+        
+        return result
+
